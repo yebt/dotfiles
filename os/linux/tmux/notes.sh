c5aecc8ab8599d569f895e85004e44463ef28519
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-09-13 15:39:36 -0500

    Feat: add tmux config

---
+++ os/linux/tmux/notes.sh
@@ -0,0 +1,64 @@
+################
+## TMUX CONFIG #
+################
+
+# GUIDE
+##########################################
+
+
+# Key binding
+##########################################
+
+### Session
+# KEYS		|->| Action
+# P s 		 ->  show all sessions
+# P w 		 ->  Sessions and windows preview
+# P ( 		 ->  Go to previous session
+# P ) 		 ->  Go to next session
+# P $ 		 ->  Rename session
+# P d 		 ->  Detach session
+
+### Window/Tabs
+# P c 		 ->  Create a new window
+# P n 		 ->  Go to next window
+# P p 		 ->  Go to prev window
+# P 0...9	 ->  Go to n (0...9) window
+# P l 		 ->  Toggle last active window
+# P , 		 ->  Rename window
+# P & 		 ->  Close current window
+
+### Panels
+# P ; 		 ->  Toggle last active pane
+# P % 		 ->  Split Horizontal
+# P " 		 ->  Split Vertical
+# P { 		 ->  Move current pane left
+# P } 		 ->  Move current pane Right
+# P <<arrows>> 	 ->  switch to pathe in the direction
+# P q 		 ->  Show pane numbers
+# P q 0...9 	 ->  Go to the pane with the number
+# P z 		 ->  Toggle pane zoom
+# P ! 		 ->  Convert pane to windows
+# P CTRL+<<arrows>> -> Resize panel
+# P spacebar	 ->  Toggle pane layouts
+# P o		 ->  Switch to next pane
+# P x		 ->  Close current pane
+
+### a
+# P [		 ->  Enter en copy mode
+# P ]		 ->  Paste inside buffer
+
+### 
+# P m		 -> toggle mark
+# P i		 -> show info
+# P t		 -> show clock
+# P ~		 -> show messages
+
+
+
+# Commands
+##########################################
+# : swap-window -s 2 -t 1	->	Reorder window, swap window number 2(src) and 1(dst)
+# : swap-window -t 1		->	Move current window to the left by one position
+
+
+

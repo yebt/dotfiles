91d815beaccbc4b59bc50f6f616771acb4624db4
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-09-14 16:08:22 -0500

    Feat: add netrw setings

---
+++ editors/nvim/lua/settings/netrw.lua
@@ -0,0 +1,8 @@
+-- # Netrw
+vim.g.netrw_banner = 0
+--vim.g.netrw_list_hide = ",^\\.\\.\\=/\\=$"
+vim.g.netrw_use_errorwindow = 0 -- show like echo
+vim.g.netrw_winsize = 30
+vim.g.netrw_keepdir = 0
+
+

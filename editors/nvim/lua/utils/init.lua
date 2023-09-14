5bdf66a230bc29fcc99132c9d736292d4477a150
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-09-14 16:07:45 -0500

    Feat: add util module

---
+++ editors/nvim/lua/utils/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+-- if is aviable a lazy plugin
+function M.is_available(plugin)
+  local lazy_config_avail, lazy_config = pcall(require, 'lazy.core.config')
+  return lazy_config_avail and lazy_config.spec.plugins[plugin] ~= nil
+end
+
+return M

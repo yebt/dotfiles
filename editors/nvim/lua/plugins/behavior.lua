3fda78b2b1807749e1874efc41ef88054149a235
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-09-15 16:05:20 -0500

    Feat: add behavior

---
+++ editors/nvim/lua/plugins/behavior.lua
@@ -0,0 +1,15 @@
+return {
+  -- Better practices
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  },
+
+  -- Colorize view
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "User AstroFile",
+    cmd = { "ColorizerToggle", "ColorizerAttachToBuffer", "ColorizerDetachFromBuffer", "ColorizerReloadAllBuffers" },
+    opts = { user_default_options = { names = false } },
+  },
+}

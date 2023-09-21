0253d99a0484a423acc235b1bc0a6ec293ab399a
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-09-21 11:12:28 -0500

    Feat: add gmtm util to use gum for commit messages

---
+++ bin/gmtm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if ! command -v gum &>/dev/null; then
+    echo "gum is not installed :C"
+    echo "see: https://github.com/charmbracelet/gum"
+    exit 1
+fi
+
+TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
+SCOPE=$(gum input --placeholder "scope")
+
+# Since the scope is optional, wrap it in parentheses if it has a value.
+test -n "$SCOPE" && SCOPE="($SCOPE)"
+
+# Pre-populate the input with the type(scope): so that the user may change it
+SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
+DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
+
+# Commit these changes
+gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"

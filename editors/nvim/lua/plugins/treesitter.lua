708bc74d1dd138169922837087ad3b5e47e7c8c1
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-09-25 14:45:46 -0500

    Fix: remove indent of treesitter

--- editors/nvim/lua/plugins/treesitter.lua
+++ editors/nvim/lua/plugins/treesitter.lua
@@ -21,7 +21,7 @@ return {
     ---@type TSConfig
     opts = {
       highlight = { enable = true },
-      indent = { enable = true },
+      indent = { enable = false },
       ensure_installed = {
         'bash',
         'c',
